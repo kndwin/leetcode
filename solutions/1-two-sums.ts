60991766e41387a3bab19d935b6ee209bff6028b
Kevin Nguyen <me@kndwin.dev>
2020-09-04 10:30:25 +1000

    Added helpful comments

--- solutions/1-two-sums.ts
+++ solutions/1-two-sums.ts
@@ -1,3 +1,4 @@
+// O(n^2) + Immutable
 function twoSum(nums: number[], target: number): number[] {
     let ans: number[] = []
     for (let i=0; i<nums.length; i++) {
@@ -5,8 +6,7 @@ function twoSum(nums: number[], target: number): number[] {
         delete tmp[i]
         for (let j=0; j<tmp.length; j++) {
             if (tmp[j]+nums[i] == target) {
-                ans.push(j, i)
-                    
+                ans.push(j, i) 
                 return ans
             }
         }
