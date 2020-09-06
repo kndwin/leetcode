813b5147a8d538ad28cc8c356dd41ffb6b0cfe00
Kevin Nguyen <me@kndwin.dev>
2020-09-06 18:01:54 +1000

    Add O(n) solution

--- solutions/1-two-sums.ts
+++ solutions/1-two-sums.ts
@@ -13,3 +13,21 @@ function twoSum(nums: number[], target: number): number[] {
     }   
     return ans
 };
+
+// O(n) + Maps
+function twoSum(nums: number[], target: number): number[] {
+    // initialise tables
+    let map = new Map()
+    let comp : number
+    // loop through array
+    for (let i=0; i<nums.length; i++) {
+        // if comp is found, return index pair
+        comp = target - nums[i]
+        if (map.get(comp) != undefined) {
+            return [i, map.get(comp)]
+        }
+        // else add to map
+        map.set(nums[i], i)
+    }
+    return [0]
+};
