34c5df50beec9c3b292754983086bb404bf13498
Kevin Nguyen <me@kndwin.dev>
2021-02-25 18:19:49 +1100

    ➕ problem 852

---
+++ solutions/852.peak-index-in-a-mountain-array/852.peak-index-in-a-mountain-array.go
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=852 lang=golang
+ *
+ * [852] Peak Index in a Mountain Array
+ *
+ * https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+ *
+ * algorithms
+ * Easy (71.74%)
+ * Total Accepted:    209.6K
+ * Total Submissions: 292.3K
+ * Testcase Example:  '[0,1,0]'
+ *
+ * Let's call an array arr a mountain if the following properties hold:
+ * 
+ * 
+ * arr.length >= 3
+ * There exists some i with 0 < i < arr.length - 1 such that:
+ * 
+ * arr[0] < arr[1] < ... arr[i-1] < arr[i] 
+ * arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+ * 
+ * 
+ * 
+ * 
+ * Given an integer array arr that is guaranteed to be a mountain, return any i
+ * such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... >
+ * arr[arr.length - 1].
+ * 
+ * 
+ * Example 1:
+ * Input: arr = [0,1,0]
+ * Output: 1
+ * Example 2:
+ * Input: arr = [0,2,1,0]
+ * Output: 1
+ * Example 3:
+ * Input: arr = [0,10,5,2]
+ * Output: 1
+ * Example 4:
+ * Input: arr = [3,4,5,1]
+ * Output: 2
+ * Example 5:
+ * Input: arr = [24,69,100,99,79,78,67,36,26,19]
+ * Output: 2
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 3 <= arr.length <= 10^4
+ * 0 <= arr[i] <= 10^6
+ * arr is guaranteed to be a mountain array.
+ * 
+ * 
+ * 
+ * Follow up: Finding the O(n) is straightforward, could you find an O(log(n))
+ * solution?
+ */
+func peakIndexInMountainArray(arr []int) int {
+
+	var isMax = false
+	var min int = 0
+	var max int = len(arr)
+	var i int = len(arr)/2;
+	fmt.Println(i)	
+
+	for !isMax { 
+		if arr[i] > arr[i+1] && arr[i-1] < arr[i] { // if max, break
+			isMax = true
+		} else if arr[i] > arr[i+1] { // greater than max, half
+			max = i
+			i = i - (max-min)/2
+		} else if arr[i-1] < arr[i] { // less than max, double
+			min = i
+			i = i + (max-min)/2
+		}
+	}
+
+	return i
+}
